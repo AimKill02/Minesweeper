e2d40735be0e3983381e308c9da54266efb8c9cd
benrui1234 <benruiavellino@gmail.com>
2025-05-14 15:10:59 +0700

    update 2

--- src/Gameplay/MSTable.java
+++ src/Gameplay/MSTable.java
@@ -5,6 +5,6 @@ public class MSTable {
     int row;
     int col;
     int[][] grids;
-    HashMap<Integer, Boolean> flagged = new HashMap<Integer, Boolean>();
-    HashMap<Integer, Boolean> revealed = new HashMap<Integer, Boolean>();
+    HashMap<Integer, Boolean> flagged = new HashMap<>();
+    HashMap<Integer, Boolean> revealed = new HashMap<>();
 }
