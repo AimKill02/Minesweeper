6daf3939a2d77a06620211742a3a074300dc6381
benrui1234 <benruiavellino@gmail.com>
2025-05-14 15:09:01 +0700

    update 2

---
+++ src/Gameplay/MSTable.java
@@ -0,0 +1,10 @@
+package Gameplay;
+import java.util.HashMap;
+
+public class MSTable {
+    int row;
+    int col;
+    int[][] grids;
+    HashMap<Integer, Boolean> flagged = new HashMap<Integer, Boolean>();
+    HashMap<Integer, Boolean> revealed = new HashMap<Integer, Boolean>();
+}
